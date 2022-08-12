511e147860e2f50d8726694c346af37ee63b6ba5
attila kekesi <kekesi.att@gmail.com>
2022-08-13 00:13:20 +0200

    created ex02

---
+++ C_Piscine/C12/ex02/main.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akekesi <akekesi@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/12 23:55:56 by akekesi           #+#    #+#             */
+/*   Updated: 2022/08/12 23:55:56 by akekesi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+#include <stdio.h>
+
+int	main(void)
+{
+	t_list	*elem1;
+	t_list	*elem2;
+	t_list	*elem3;
+	void	*data;
+
+	data = NULL;
+	elem1 = ft_create_elem(data);
+	elem2 = ft_create_elem(data);
+	elem3 = ft_create_elem(data);
+	elem1 -> next = elem2;
+	elem2 -> next = elem3;
+	printf("size: %d\n", ft_list_size(elem1));
+	printf("size: %d\n", ft_list_size(elem2));
+	printf("size: %d\n", ft_list_size(elem3));
+	printf("size: %d\n", ft_list_size(elem2));
+	printf("size: %d\n", ft_list_size(elem1));
+}
