dcf0edae71f187766d4f27a7cf88ac0621ea915b
attila kekesi <kekesi.att@gmail.com>
2022-08-13 00:42:53 +0200

    fixed spelling

--- C_Piscine/C12/ex01/main.c
+++ C_Piscine/C12/ex01/main.c
@@ -38,14 +38,14 @@ int	main(void)
 	elem1 = ft_create_elem(data);
 	begin_list = &elem1;
 	
-	printf("*elem1:        %p <-- %p <-- %p \n", &elem1->next, elem1, &elem1);
-	printf("**beginn_list: %p <-- %p <-- %p <-- %p\n", &(*begin_list) -> next, *begin_list, begin_list, &begin_list);
+	printf("*elem1:       %p <-- %p <-- %p \n", &elem1->next, elem1, &elem1);
+	printf("**begin_list: %p <-- %p <-- %p <-- %p\n", &(*begin_list) -> next, *begin_list, begin_list, &begin_list);
 	
 	ft_list_push_front(begin_list, data);
-	printf("**beginn_list: %p <-- %p <-- %p <-- %p\n", &(*begin_list) -> next, *begin_list, begin_list, &begin_list);
+	printf("**begin_list: %p <-- %p <-- %p <-- %p\n", &(*begin_list) -> next, *begin_list, begin_list, &begin_list);
 
 	free(elem1);
 	elem1 = NULL;
-	printf("*elem1:        %p <-- %p <-- %p \n", &elem1->next, elem1, &elem1);
-	printf("**beginn_list: %p <-- %p <-- %p <-- %p\n", &(*begin_list) -> next, *begin_list, begin_list, &begin_list);
+	printf("*elem1:       %p <-- %p <-- %p \n", &elem1->next, elem1, &elem1);
+	printf("**begin_list: %p <-- %p <-- %p <-- %p\n", &(*begin_list) -> next, *begin_list, begin_list, &begin_list);
 }
