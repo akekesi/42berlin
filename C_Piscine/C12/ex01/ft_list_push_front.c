be2fedd3f41f1b9e284603c96989c2c06504c2d7
attila kekesi <kekesi.att@gmail.com>
2022-08-13 10:17:56 +0200

    added *beginn_list = NULL case

--- C_Piscine/C12/ex01/ft_list_push_front.c
+++ C_Piscine/C12/ex01/ft_list_push_front.c
@@ -17,6 +17,7 @@ void	ft_list_push_front(t_list **begin_list, void *data)
 	t_list	*elem;
 
 	elem = ft_create_elem(data);
-	elem -> next = *begin_list;
+	if (*begin_list)
+		elem -> next = *begin_list;
 	*begin_list = elem;
 }
