b01bf6e368d7eb32cd18d4c8ce8b5ab4e50cf8c5
Attila Kekesi <akekesi@c4c4c3.42berlin.de>
2022-12-13 20:05:59 +0100

    added new function and header

--- Curriculum/00_libft/01_code/libft.h
+++ Curriculum/00_libft/01_code/libft.h
@@ -1,7 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akekesi <akekesi@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/13 14:22:38 by akekesi           #+#    #+#             */
+/*   Updated: 2022/12/13 19:11:10 by akekesi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBFT_H
 # define LIBFT_H
 
 # include <string.h>
+# include <stdlib.h>
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -14,5 +27,6 @@ int		ft_tolower(int c);
 void	*ft_memset(void *str, int c, size_t n);
 void	ft_bzero(void *str, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 
 #endif
