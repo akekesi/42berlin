759b508259cc336281430eef81c0e0f06db7950b
attila kekesi <kekesi.att@gmail.com>
2022-12-18 14:02:07 +0100

    updated test

--- Curriculum/00_libft/01_code/test_ft_strlcpy.c
+++ Curriculum/00_libft/01_code/test_ft_strlcpy.c
@@ -44,28 +44,28 @@ static int	test_check(int result, int result_ft, char * dest, char *dest_ft, int
 		printf("Error-%d:\n", n);
 		printf("result:    %d\n", result);
 		printf("result_ft: %d\n", result_ft);
-		printf("dest:    %s\n", dest);
-		printf("dest_ft:    %s\n", dest_ft);
+		printf("dest:      %s\n", dest);
+		printf("dest_ft:   %s\n", dest_ft);
 		return (0);
 	}
 	return (1);
 }
 
-static size_t	sof_strlcpy(char *dst, const char *src, size_t siz)
+static size_t	sof_strlcpy(char *dst, const char *src, size_t size)
 {
 	char *d = dst;
 	const char *s = src;
-	size_t n = siz;
+	size_t n = size;
 	/* Copy as many bytes as will fit */
 	if (n != 0) {
 		while (--n != 0) {
 			if ((*d++ = *s++) == '\0')
 				break;
 		}
-  }
+	}
 	/* Not enough room in dst, add NUL and traverse rest of src */
 	if (n == 0) {
-		if (siz != 0)
+		if (size != 0)
 			*d = '\0';		/* NUL-terminate dst */
 		while (*s++)
 			;
