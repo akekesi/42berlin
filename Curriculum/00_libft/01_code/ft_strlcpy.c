d70da4c251852b6b313497ea14215f32eca95abc
akekesi <kekesi.att@gmail.com>
2022-12-22 18:11:37 +0100

    fixed strlcpy

--- Curriculum/00_libft/01_code/ft_strlcpy.c
+++ Curriculum/00_libft/01_code/ft_strlcpy.c
@@ -6,38 +6,25 @@
 /*   By: akekesi <akekesi@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 11:54:12 by akekesi           #+#    #+#             */
-/*   Updated: 2022/12/18 15:43:33 by akekesi          ###   ########.fr       */
+/*   Updated: 2022/12/22 18:10:43 by akekesi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-size_t		ft_strlcpy(char *dest, const char *src, size_t n);
-static int	hf_strlen(char *str);
 
 size_t	ft_strlcpy(char *dest, const char *src, size_t n)
 {
 	int	i;
 	int	len_src;
 
-	len_src = hf_strlen((char *)src);
+	len_src = ft_strlen((char *)src);
 	i = 0;
-	while ((i < ((int)n - 1)) && src[i])
+	if (len_src + 1 < n)
+		ft_memcpy(dest, src, len_src + 1);
+	else if (n)
 	{
-		dest[i] = src[i];
-		i++;
+		ft_memcpy(dest, src, n - 1);
+		dest[n - 1] = '\0';
 	}
-	if (n)
-		dest[i] = '\0';
 	return (len_src);
-}
-
-static int	hf_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
 }
