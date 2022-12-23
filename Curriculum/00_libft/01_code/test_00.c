c9458a68f136df3c68b858ee5dc27d43a1df64a7
akekesi <kekesi.att@gmail.com>
2022-12-23 17:22:25 +0100

    updated test_00

--- Curriculum/00_libft/01_code/test_00.c
+++ Curriculum/00_libft/01_code/test_00.c
@@ -1,4 +1,5 @@
 #include "stdio.h"
+#include <stdlib.h>
 
 int	main(void)
 {
@@ -17,5 +18,14 @@ int	main(void)
 	str = "";
 	if (str)
 		printf("1");
+	// -------------
+	char	*ptr_char;
+	ptr_char = (char *)malloc(0);
+	printf("%p --> ***%c***\n", ptr_char, *ptr_char + 'A');
+	char	*ptr_void;
+	ptr_void = malloc(0);
+	printf("%p --> ***%c***\n", ptr_void, *ptr_void + 'B');
+	// -------------
+
 	return (1);
 }
