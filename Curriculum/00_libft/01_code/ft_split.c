27709f5dd763dd24c97f5c4b501e32b0746994f1
attila kekesi <kekesi.att@gmail.com>
2022-12-25 01:01:00 +0100

    split: fixed norminette (splitting function)

--- Curriculum/00_libft/01_code/ft_split.c
+++ Curriculum/00_libft/01_code/ft_split.c
@@ -14,39 +14,49 @@
 #include "test.h"
 
 char		**ft_split(char const *str, char const c);
+static char	**hf_ft_split(char const *str, char const c, char **strs, int m);
 static int	hf_count_words(char const *str, char const c);
 static int	hf_len_word(char const *str, char const c, int i);
 static void	hf_free_matrix(char **matrix);
 
 char	**ft_split(char const *str, char const c)
 {
-	int		i;
 	int		m;
-	int		m_max;
-	int		n;
 	char	**strs;
 
-	m_max = hf_count_words(str, c);
-	strs = (char **)malloc(sizeof(char *) * (m_max + 1));
+	m = hf_count_words(str, c);
+	strs = (char **)malloc(sizeof(char *) * (m + 1));
 	if (!strs)
 		return (NULL);
+	strs = hf_ft_split(str, c, strs, m);
+	if (!str)
+		return (NULL);
+	return (strs);
+}
+
+static char	**hf_ft_split(char const *str, char const c, char **strs, int m_max)
+{
+	int		i;
+	int		m_;
+	int		n;
+
 	i = 0;
-	m = 0;
-	while (m < m_max)
+	m_ = 0;
+	while (m_ < m_max)
 	{
 		while (str[i] == c)
 			i++;
 		n = hf_len_word(str, c, i);
-		strs[m] = ft_substr(str, i, n);
-		if (!strs[m])
+		strs[m_] = ft_substr(str, i, n);
+		if (!strs[m_])
 		{
 			hf_free_matrix(strs);
 			return (NULL);
 		}
 		i += n;
-		m++;
+		m_++;
 	}
-	strs[m] = NULL;
+	strs[m_] = NULL;
 	return (strs);
 }
 
