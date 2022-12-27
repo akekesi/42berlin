2b3e50c89ebe1fad0577c72992f868ace16678bf
akekesi <kekesi.att@gmail.com>
2022-12-27 17:14:47 +0100

    lstsize: deleted tmp

--- Curriculum/00_libft/01_code/ft_lstsize_bonus.c
+++ Curriculum/00_libft/01_code/ft_lstsize_bonus.c
@@ -6,7 +6,7 @@
 /*   By: akekesi <akekesi@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 01:37:52 by akekesi           #+#    #+#             */
-/*   Updated: 2022/12/26 16:54:34 by akekesi          ###   ########.fr       */
+/*   Updated: 2022/12/27 17:13:45 by akekesi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,12 @@
 int	ft_lstsize(t_list *lst)
 {
 	int		i;
-	t_list	*tmp;
 
 	i = 0;
-	tmp = lst;
-	while (tmp)
+	while (lst)
 	{
 		i++;
-		tmp = tmp->next;
+		lst = lst->next;
 	}
 	return (i);
 }
