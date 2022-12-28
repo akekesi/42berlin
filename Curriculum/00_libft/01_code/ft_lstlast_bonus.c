d62d77ef803092d29da09e829a675f50225ece8c
akekesi <kekesi.att@gmail.com>
2022-12-28 16:57:16 +0100

    lstlast: deleted last

--- Curriculum/00_libft/01_code/ft_lstlast_bonus.c
+++ Curriculum/00_libft/01_code/ft_lstlast_bonus.c
@@ -6,7 +6,7 @@
 /*   By: akekesi <akekesi@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 12:11:11 by akekesi           #+#    #+#             */
-/*   Updated: 2022/12/26 16:55:58 by akekesi          ###   ########.fr       */
+/*   Updated: 2022/12/28 16:44:20 by akekesi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,9 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
-	t_list	*last;
-
 	if (!lst)
 		return (NULL);
-	last = lst;
-	while (last->next)
-		last = last->next;
-	return (last);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
 }
