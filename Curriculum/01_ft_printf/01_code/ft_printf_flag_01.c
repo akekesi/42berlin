5e903765162b89245ab63a43181666fc080f0bc9
attila kekesi <kekesi.att@gmail.com>
2023-01-04 23:47:17 +0100

    printf_malloc: delete . from ptr in flag_01

--- Curriculum/01_ft_printf/01_code/ft_printf_flag_01.c
+++ Curriculum/01_ft_printf/01_code/ft_printf_flag_01.c
@@ -56,12 +56,6 @@ int	ft_print_ptr_flag(va_list *args, int *flags_info)
 
 	n = 0;
 	arg = ft_ptr_to_str(va_arg(*args, unsigned long long));
-	if (flags_info[2])
-	{
-		dot_int = flags_info[8];
-		if (dot_int < ft_str_len(arg))
-			arg[dot_int] = '\0';
-	}
 	n += ft_print_pad_pos(arg, flags_info);
 	free(arg);
 	return (n);
