6ea342c3a51b4c7b2d42e1cca89dd9a3abd82e02
akekesi <kekesi.att@gmail.com>
2023-01-13 00:32:50 +0100

    gnl: deleted comments adn debug

--- Curriculum/02_get_next_line/01_code/get_next_line.c
+++ Curriculum/02_get_next_line/01_code/get_next_line.c
@@ -6,16 +6,11 @@
 /*   By: akekesi <akekesi@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 16:32:34 by akekesi           #+#    #+#             */
-/*   Updated: 2023/01/13 00:17:00 by akekesi          ###   ########.fr       */
+/*   Updated: 2023/01/13 00:31:45 by akekesi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include "test.h"
-
-/*
-read(\n) = 0 ???
-*/
 
 char	*get_next_line(int fd)
 {
@@ -24,9 +19,6 @@ char	*get_next_line(int fd)
 	char		*str_buff;
 	char		*str_temp;
 	static char	*str_line;
-
-	int			debug;
-	debug = 0;
 
 	if (fd < 0 || BUFFER_SIZE < 1)
 	{
@@ -52,31 +44,18 @@ char	*get_next_line(int fd)
 		return (NULL);
 	}
 	str_buff[n] = '\0';
-	if (debug)
-		printf("(%d-%s): ", (int)n, str_buff);
 	if (n == -1)
-	{	// n = -1 --> error
-		if (debug)
-			printf("n = -1*");
+	{
 		free (str_buff);
 		return (NULL);
 	}
 	else if (n == 0)
-	{	// n = 0  --> nothing was read
-		if (debug)
-			printf("n = 0 and ");
+	{
 		free (str_buff);
 		if (ft_str_len(str_line))
-		{	// str_line not empty
+		{
 			if (ft_str_char(str_line, '\n'))
-			{	// \n in str_line
-				/*
-				-copy text until \n or \0 from str_line in str_rtrn and return(str_rtrn)
-				-save rest in str_line
-				-use str_temp to free old str_line
-				*/
-				if (debug)
-					printf("with \\n*");
+			{
 				str_temp = str_line;
 				str_rtrn = ft_str_dup_b(str_temp, '\n');
 				str_line = ft_str_dup_a(str_temp, '\n');
@@ -84,13 +63,7 @@ char	*get_next_line(int fd)
 				return (str_rtrn);
 			}
 			else
-			{	// \n not in str_line
-				/*
-				-copy text until \0 from str_line in str_rtrn and return(str_rtrn)
-				-free str_line and null
-				*/
-				if (debug)
-					printf("without \\n*");
+			{
 				str_rtrn = ft_str_dup_b(str_line, '\0');
 				free (str_line);
 				str_line = NULL;
@@ -98,37 +71,20 @@ char	*get_next_line(int fd)
 			}
 		}
 		else
-		{	// str_line empty
-			if (debug)
-				printf("empty*");
+		{
 			free (str_line);
 			str_line = NULL;
 			return (str_line);
 		}
 	}
 	else
-	{	// n > 0  --> something was read
-		/*
-		-not end of the text
-		-concatenate str_line and str_buff
-		-free str_buff
-		-use str_temp to free old str_line
-		*/
-		if (debug)
-			printf("n > 0 and ");
+	{
 		str_temp = str_line;
 		str_line = ft_str_cat(str_temp, str_buff);
 		free(str_temp);
 		free(str_buff);
 		if (ft_str_char(str_line, '\n'))
-		{	// \n in str_line
-			/*
-			-copy text until \n or \0 from str_line in str_rtrn and return(str_rtrn)
-			-save rest in str_line
-			-use str_temp to free old str_line
-			*/
-			if (debug)
-				printf("with \\n*");
+		{
 			str_temp = str_line;
 			str_rtrn = ft_str_dup_b(str_temp, '\n');
 			str_line = ft_str_dup_a(str_temp, '\n');
@@ -136,13 +92,6 @@ char	*get_next_line(int fd)
 			return (str_rtrn);
 		}
 		else
-		{	// \n not in str_line
-			/*
-			-call get_next_line() again (recursive)
-			*/
-			if (debug)
-				printf("without \\n*");
 			return (get_next_line(fd));
-		}
 	}
 }
