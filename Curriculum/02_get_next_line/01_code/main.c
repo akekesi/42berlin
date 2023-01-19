51ca267383124d49d2884ef68b8251912b7e13af
akekesi <kekesi.att@gmail.com>
2023-01-19 15:23:43 +0100

    tidied up tests in main

--- Curriculum/02_get_next_line/01_code/main.c
+++ Curriculum/02_get_next_line/01_code/main.c
@@ -6,7 +6,7 @@
 /*   By: akekesi <akekesi@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 15:17:59 by akekesi           #+#    #+#             */
-/*   Updated: 2023/01/17 21:26:26 by akekesi          ###   ########.fr       */
+/*   Updated: 2023/01/19 15:22:57 by akekesi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,41 +21,45 @@ int	main(void)
 	// ulimit -H -n
 
 	printf("|-- TEST -->\n");
-	printf("|-- get_next_line -->\n");
-	// gcc main.c test.c get_next_line.c get_next_line_utils.c&& valgrind ./a.out
-	int		i;
-	int		fd;
-	char	*str;
-	char	*path;
+	// printf("|-- get_next_line -->\n");
+	// // gcc main.c test.c get_next_line.c get_next_line_utils.c&& valgrind ./a.out
+	// int		i;
+	// int		fd;
+	// char	*str;
+	// char	*path;
 
-	path = "99_text/multiple_nl.txt";
-	fd = open(path, O_RDONLY);
-	i = 0;
-	while (i < 6) // <-- number of loops
-	{
-		i++;
-		str = get_next_line(fd);
-		printf("%d. -->%s<--\n", i, str);
-		free(str);
-	}
-	printf("<-- get_next_line --|\n");
+	// path = "99_text/multiple_nl.txt";
+	// fd = open(path, O_RDONLY);
+	// i = 0;
+	// while (i < 6) // <-- number of loops
+	// {
+	// 	i++;
+	// 	str = get_next_line(fd);
+	// 	printf("%d. -->%s<--\n", i, str);
+	// 	free(str);
+	// }
+	// printf("<-- get_next_line --|\n");
 
 	// printf("|-- get_next_line loop -->\n");
 	// // gcc main.c test.c get_next_line.c get_next_line_utils.c&& valgrind ./a.out
 	// test_gnl_loop(1);
+	// printf("-------------\n");
 	// test_gnl_loop(0);
+	// printf("-------------\n");
 	// test_gnl_loop(1);
 	// printf("<-- get_next_line loop --|\n");
 
 	// printf("|-- get_next_line new start -->\n");
 	// // gcc main.c test.c get_next_line.c get_next_line_utils.c&& valgrind ./a.out
 	// test_gnl_new_start(1);
+	// printf("-------------\n");
 	// test_gnl_new_start(0);
+	// printf("-------------\n");
 	// test_gnl_new_start(1);
 	// printf("<-- get_next_line new start --|\n");
 
 	// printf("|-- get_next_line bonus -->\n");
-	// // gcc main.c test.c *bonus* && valgrind ./a.out
+	// // gcc main.c test.c *bonus.c && valgrind ./a.out
 	// test_gnl_bonus(1);
 	// printf("<-- get_next_line bonus --|\n");
 	printf("<-- TEST --|\n");
