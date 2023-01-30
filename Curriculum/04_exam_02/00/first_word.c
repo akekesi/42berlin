af3f3f7a52a79c8fc3eb33ab294b486db17776e1
kekesi <kekesi.att@gmail.com>
2023-01-30 18:54:08 +0100

    exam_02: created first_word.c, fizzbuzz.c and ft_swap.c

---
+++ Curriculum/04_exam_02/00/first_word.c
@@ -0,0 +1,53 @@
+/*
+Assignment name  : first_word
+Expected files   : first_word.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes a string and displays its first word, followed by a
+newline.
+
+A word is a section of string delimited by spaces/tabs or by the start/end of
+the string.
+
+If the number of parameters is not 1, or if there are no words, simply display
+a newline.
+
+Examples:
+
+$> ./first_word "FOR PONY" | cat -e
+FOR$
+$> ./first_word "this        ...       is sparta, then again, maybe    not" | cat -e
+this$
+$> ./first_word "   " | cat -e
+$
+$> ./first_word "a" "b" | cat -e
+$
+$> ./first_word "  lorem,ipsum  " | cat -e
+lorem,ipsum$
+$>
+*/
+
+#include <unistd.h>
+
+void	ft_first_word(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\t')
+		i++;
+	while (str[i] && str[i] != ' ' && str[i] != '\t')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+		ft_first_word(argv[1]);
+	write(1, "\n", 1);
+}
+
---
+++ Curriculum/04_exam_02/00/fizzbuzz.c
@@ -0,0 +1,79 @@
+/*
+Assignment name  : fizzbuzz
+Expected files   : fizzbuzz.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that prints the numbers from 1 to 100, each separated by a
+newline.
+
+If the number is a multiple of 3, it prints 'fizz' instead.
+
+If the number is a multiple of 5, it prints 'buzz' instead.
+
+If the number is both a multiple of 3 and a multiple of 5, it prints 'fizzbuzz' instead.
+
+Example:
+
+$>./fizzbuzz
+1
+2
+fizz
+4
+buzz
+fizz
+7
+8
+fizz
+buzz
+11
+fizz
+13
+14
+fizzbuzz
+[...]
+97
+98
+fizz
+buzz
+$> 
+*/
+
+#include <unistd.h>
+
+void	ft_write_int(int n)
+{
+	char	c;
+
+	if (n > 9)
+	{
+		ft_write_int(n / 10);
+		n = n % 10;
+	}
+	c = n + '0';
+	write(1, &c, 1);
+}
+
+void	ft_fizzbuzz(void)
+{
+	int	i;
+
+	i = 1;
+	while (i < 101)
+	{
+		if (i % 3 == 0)
+			write(1, "fizz", 4);
+		if (i % 5 == 0)
+			write(1, "buzz", 4);
+		if (i % 3 != 0 && i % 5 != 0)
+			ft_write_int(i);
+		write(1, "\n", 1);
+		i++;
+	}
+}
+
+int	main(void)
+{
+	ft_fizzbuzz();
+}
+
