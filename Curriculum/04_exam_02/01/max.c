52a17002922e8dc8c17048c19583418cd6194d63
kekesi <kekesi.att@gmail.com>
2023-02-02 10:13:07 +0100

    exam_02: created mac.c

--- Curriculum/04_exam_02/01/max.c
+++ Curriculum/04_exam_02/01/max.c
@@ -16,3 +16,35 @@ The function returns the largest number found in the array.
 If the array is empty, the function returns 0.
 */
 
+#include <stdio.h>
+
+int	max(int *tab, unsigned int len)
+{
+	int	tmp;
+
+	if (!len)
+		return (0);
+	tmp = tab[--len];
+	while (len)
+	{
+		len--;
+		if (tab[len] > tmp)
+			tmp = tab[len];
+	}
+	return (tmp);
+}
+
+int	main(void)
+{
+	int	numbers1[9] = {-100, -11, -1, 0, 1, 11, 13, 42, 999};
+	int	numbers2[9] = {100, 11, 111, 1010, 1, 1010, 13, 42, -999};
+	int	numbers3[0] = {};
+	int	numbers4[1] = {13};
+	int	numbers5[2] = {1, 0};
+
+	printf("%d\n", max(numbers1, 9));
+	printf("%d\n", max(numbers2, 9));
+	printf("%d\n", max(numbers3, 0));
+	printf("%d\n", max(numbers4, 1));
+	printf("%d\n", max(numbers5, 2));
+}
