bf3d65e4cde0740af86b7758428457643f83f07c
kekesi <kekesi.att@gmail.com>
2023-02-03 22:05:31 +0100

    exam_02: created add_prime_sum.c

--- Curriculum/04_exam_02/03/add_prime_sum.c
+++ Curriculum/04_exam_02/03/add_prime_sum.c
@@ -22,3 +22,76 @@ $>./add_prime_sum | cat -e
 0$
 $>
 */
+
+#include <unistd.h>
+
+int	ft_atoi_pos(char *str)
+{
+	int	i;
+	int	n;
+
+	i = 0;
+	n = 0;
+	while (str[i])
+	{
+		n = n * 10 + str[i] - '0';
+		i++;
+	}
+	return (n);
+}
+
+int	ft_isprime(int n)
+{
+	int	d;
+
+	d = 2;
+	if (n < 2)
+		return (0);
+	while (d < n)
+	{
+		if (!(n % d))
+			return (0);
+		d++;
+	}
+	return (1);
+}
+
+void	ft_putnbr(int n)
+{
+	char	c;
+
+	if (n > 9)
+		ft_putnbr(n / 10);
+	c = n % 10 + '0';
+	write(1, &c, 1);
+}
+
+int	ft_add_prime_sum(char *str)
+{
+	int	n;
+	int	s;
+
+	n = ft_atoi_pos(str);
+	s = n;
+	while (n > 1)
+	{
+		n--;
+		if (ft_isprime(n))
+			s += n;
+	}
+	ft_putnbr(s);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		if (argv[1][0] != '-')
+			ft_add_prime_sum(argv[1]);
+		else
+			write(1, "0", 1);
+	}
+	else
+		write(1, "0", 1);
+	write(1, "\n", 1);
+}
