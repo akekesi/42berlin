3881e42cd0b095b27eaa8eef87980d0fca3116fc
akekesi <kekesi.att@gmail.com>
2023-09-08 15:03:31 +0200

    phil: added time consuming part to get_fork

--- Curriculum/08_philosophers/01_code/do_eat.c
+++ Curriculum/08_philosophers/01_code/do_eat.c
@@ -6,7 +6,7 @@
 /*   By: akekesi <akekesi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 20:06:29 by akekesi           #+#    #+#             */
-/*   Updated: 2023/09/07 23:08:04 by akekesi          ###   ########.fr       */
+/*   Updated: 2023/09/08 14:38:33 by akekesi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,33 @@ void	do_eat(t_phil **phil)
 
 void	get_fork_left(t_phil **phil)
 {
+	struct timeval	time;
+	int				time_0;
+	int				time_1;
+
+	gettimeofday(&time, NULL);
+	time_0 = time.tv_sec * 1000 + time.tv_usec / 1000;
 	while (1)
 	{
+		time_1 = get_time(time_0);
+		if (10 < time_1)
+		{
+			// printf("----->%d %d %d\n", (*phil)->n, (*phil)->time_rest, time_1);
+			if (check_die(phil))
+				return ;
+			if ((*phil)->time_rest < time_1)
+			{
+				pthread_mutex_lock(&(*phil)->info->lock);
+				(*phil)->info->die = 1;
+				pthread_mutex_unlock(&(*phil)->info->lock);
+				print(phil, "died");
+				(*phil)->time_rest = 0;
+				return ;
+			}
+			(*phil)->time_rest -= time_1;
+			gettimeofday(&time, NULL);
+			time_0 = time.tv_sec * 1000 + time.tv_usec / 1000;
+		}
 		pthread_mutex_lock(&(*phil)->left->lock);
 		if ((*phil)->left->free)
 		{
@@ -49,8 +74,33 @@ void	get_fork_left(t_phil **phil)
 
 void	get_fork_right(t_phil **phil)
 {
+	struct timeval	time;
+	int				time_0;
+	int				time_1;
+
+	gettimeofday(&time, NULL);
+	time_0 = time.tv_sec * 1000 + time.tv_usec / 1000;
 	while (1)
 	{
+		time_1 = get_time(time_0);
+		if (10 < time_1)
+		{
+			// printf("--THIS--->%d %d\n", (*phil)->time_rest, time_1);
+			if (check_die(phil))
+				return ;
+			if ((*phil)->time_rest < time_1)
+			{
+				pthread_mutex_lock(&(*phil)->info->lock);
+				(*phil)->info->die = 1;
+				pthread_mutex_unlock(&(*phil)->info->lock);
+				print(phil, "died");
+				(*phil)->time_rest = 0;
+				return ;
+			}
+			(*phil)->time_rest -= time_1; 
+			gettimeofday(&time, NULL);
+			time_0 = time.tv_sec * 1000 + time.tv_usec / 1000;
+		}
 		pthread_mutex_lock(&(*phil)->right->lock);
 		if ((*phil)->right->free)
 		{
--- Curriculum/08_philosophers/01_code/routine.c
+++ Curriculum/08_philosophers/01_code/routine.c
@@ -6,7 +6,7 @@
 /*   By: akekesi <akekesi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 17:59:02 by akekesi           #+#    #+#             */
-/*   Updated: 2023/09/07 23:07:35 by akekesi          ###   ########.fr       */
+/*   Updated: 2023/09/08 15:01:51 by akekesi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,14 @@ void	*routine(void *arg)
 	i = 0;
 	while (i < phil->n_eat || phil->n_eat == -1)
 	{
-		do_eat(&phil);
-		do_sleep(&phil);
-		do_think(&phil);
+		if (!check_die(&phil))
+			do_eat(&phil);
+		if (!check_die(&phil))
+			do_sleep(&phil);
+		if (!check_die(&phil))
+			do_think(&phil);
+		if (check_die(&phil))
+			break ;
 		i++;
 	}
 	return (NULL);
