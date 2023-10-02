0d1f0d190971ea803b8830704ab82c1c94d53838
akekesi <kekesi.att@gmail.com>
2023-10-03 00:33:57 +0200

    so_long: refactored 05_win_lose.c

--- Curriculum/07_so_long/01_code/05_win_lose.c
+++ Curriculum/07_so_long/01_code/05_win_lose.c
@@ -6,7 +6,7 @@
 /*   By: akekesi <akekesi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 17:33:32 by akekesi           #+#    #+#             */
-/*   Updated: 2023/10/02 17:23:10 by akekesi          ###   ########.fr       */
+/*   Updated: 2023/10/03 00:24:20 by akekesi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,11 +29,6 @@ void	init_img_win(t_game *game)
 	mlx_delete_texture(texture);
 }
 
-void	free_img_win(t_game *game)
-{
-	mlx_delete_image(game->mlx, game->img_win);
-}
-
 void	init_img_lose(t_game *game)
 {
 	mlx_texture_t	*texture;
@@ -44,6 +39,11 @@ void	init_img_lose(t_game *game)
 	mlx_set_instance_depth(game->img_lose->instances, 3);
 	mlx_resize_image(game->img_lose, MSG_WIDTH, MSG_HEIGHT);
 	mlx_delete_texture(texture);
+}
+
+void	free_img_win(t_game *game)
+{
+	mlx_delete_image(game->mlx, game->img_win);
 }
 
 void	free_img_lose(t_game *game)
