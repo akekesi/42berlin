7030fb4bf23e458ce0e258232505930cc99c90e1
akekesi <kekesi.att@gmail.com>
2023-10-03 21:48:33 +0200

    so_long: updated todo list

--- Curriculum/07_so_long/01_code/so_long.c
+++ Curriculum/07_so_long/01_code/so_long.c
@@ -6,25 +6,17 @@
 /*   By: akekesi <akekesi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 16:25:08 by akekesi           #+#    #+#             */
-/*   Updated: 2023/10/03 21:45:22 by akekesi          ###   ########.fr       */
+/*   Updated: 2023/10/03 21:48:11 by akekesi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 /*
 - error handling
-- delete images
 - read map (ures, ...)
-- starting position
-- exit position
-- check_map(&game->map)
 - return value int --> meg lehet szakitani a jatekot
-- collectable and win is moving back after lot of loop (because -9999 +50+50+50...)
-- valgrind should give some leaks --> but how many is OK?
-- a key_hook-ba lehetne egy find enemit es egy tobblepcsos savvaltassal elkerulni, hogy egymasra menjenek
 - show function for start, stop, win and lose
 - a price utan ne tudjon savot valtani, mert kicsit furan mozog
-...
 */
 
 void	init_sub(t_game *game, char *path_map)
